798beca0a1bedf0b2017dff05f02665fa277ceb7
Deepak Kumar <116838434+dk041102@users.noreply.github.com>
2026-03-11 14:22:41 +0530

    Add files via upload

---
+++ API Gateway/config/services.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Auth : "http://localhost:3001",
+    Order : "http://localhost:3002",
+    Product : "http://localhost:3003",
+    Payment: "http://localhost:3004",
+    Notification : "http://localhost:3005",
+
+}
