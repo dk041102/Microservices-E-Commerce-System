3f2f9ff83496f19d47d9f6d19ee6ca004b2a4657
Deepak Kumar <116838434+dk041102@users.noreply.github.com>
2026-04-07 08:48:16 +0530

    Add files via upload

--- Auth/server.js
+++ Auth/server.js
@@ -1,1 +1,18 @@
-chv
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const connectDb = require("./config/db")
+const authroutes = require("./Routes/authRoutes")
+
+dotenv.config();
+const app = express();
+app.use(cors())
+app.use(express.json())
+connectDb();
+
+//app.use("/auth", authRoutes)
+
+const Port = 3001;
+app.listen(Port,()=>{
+    console.log(`This service is working on ${Port}`)
+});
