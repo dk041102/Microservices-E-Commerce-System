df28d11298bce7fef8390d94e90d61e9cd235d9e
Deepak Kumar <116838434+dk041102@users.noreply.github.com>
2026-04-07 08:50:13 +0530

    Add files via upload

--- Auth/config/db.js
+++ Auth/config/db.js
@@ -1,1 +1,11 @@
-
+const mongoose = require("mongoose")
+const connectDb = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Mongoose is connected");
+    }catch(error){
+        console.error(error)
+        process.exit(1);
+    }
+}
+module.exports = connectDb
