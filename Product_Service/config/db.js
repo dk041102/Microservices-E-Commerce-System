3618729ff7b77873dd63c6d0c747cfcf4629a3db
Deepak Kumar <116838434+dk041102@users.noreply.github.com>
2026-04-16 08:31:23 +0530

    Create db.js

---
+++ Product_Service/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const connectDb = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database is connected")
+    }
+    catch(err){
+        console.error(err);
+        process.exit(1);
+    }
+}
+module.exports = connectDb;
